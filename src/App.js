11f1e7f4c9fde46689972c4d338b38f235a857c9
Maria Luiza Fornagieri <mm44m6@rocketmail.com>
2020-01-26 13:27:32 -0300

    changed main app screen to put input and button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Game from './Containers/Game';
 import './App.css';
 
+
 function App() {
+
+  const [boardSize, setBoardSize] = useState(0);
+  const [showGame, setShowGame] = useState(false); 
+
   return (
-    <div className="TicTacToeApp">
-      <header>Play tic tac toe with your friend!</header>
-      <Game />
+    <div className="GameSetup">
+       {showGame ?
+        <Game boardSize={parseInt(boardSize)} /> :
+        <>
+          <input className="BoardSizeInput" type="number" value={boardSize} onChange={(e) => setBoardSize(e.target.value)} />
+          <button className="StartGameButton" onClick={(e) => setShowGame(!showGame)}> Start Game</button> 
+        </>
+       }
     </div>
   );
+  
 }
 
 export default App;
