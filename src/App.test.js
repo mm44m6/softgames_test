fab51d30c185181f865413298b40f4cb6fc9c7d8
Maria Luiza Fornagieri <mm44m6@rocketmail.com>
2020-01-26 13:28:01 -0300

    changed unit tests to cover new feature

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,13 @@ beforeEach(() => {
   wrapper = shallow(<App />);
 });
 
-test('Contains the game', () => {
+test('Contains the game setup', () => {
+  expect(wrapper.find('.GameSetup')).toHaveLength(1);
+});
+
+test('Game appears after click', () => {
+  wrapper.find('input').simulate('change', { target: { value: '5' } });
+  wrapper.find('button').simulate('click');
+
   expect(wrapper.find('Game')).toHaveLength(1);
 });
