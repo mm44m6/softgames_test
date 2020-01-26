b147490bbf7529e94075038a9c6a742f39b6f3c2
Maria Luiza Fornagieri <mm44m6@rocketmail.com>
2020-01-26 13:30:07 -0300

    changed unit tests

--- src/lib/game-logic.test.js
+++ src/lib/game-logic.test.js
@@ -17,24 +17,65 @@ describe('Util Functions', () => {
 	});
 
 	test('findWinner - different winners', () => {
-		const winnerIs1 = [[1, 1, 1], [0, 2, 2], [0, 2, 0]];
-		expect(findWinner(winnerIs1)).toEqual(1);
+		let board = Array(5).fill(0);
+		for(let i = 0; i < 5; i++) {
+			board[i] = Array(5).fill(0);
+	  	}
 
-		const winnerIs2 = [[2, 1, 1], [2, 1, 1], [2, 2, 0]];
+		let winnerIs1st = board;
+		
+		winnerIs1st[0][0] = 1;
+		winnerIs1st[1][0] = 2;
+		winnerIs1st[1][1] = 1;
+		winnerIs1st[2][0] = 2;
+		winnerIs1st[2][2] = 1;
+
+		expect(findWinner(winnerIs1st)).toEqual(1);
+
+		let winnerIs2 = board;
+
+		winnerIs2[0][0] = 2;
+		winnerIs2[1][0] = 1;
+		winnerIs2[1][1] = 2;
+		winnerIs2[2][0] = 1;
+		winnerIs2[2][2] = 2;
 		expect(findWinner(winnerIs2)).toEqual(2);
 
-		const noWinner = [[1, 2, 1], [2, 1, 2], [2, 1, 2]];
+		let noWinner = board;
+
+		winnerIs2[0][0] = 2;
+		winnerIs2[1][0] = 1;
+		winnerIs2[1][1] = 2;
+		winnerIs2[2][0] = 1;
+		winnerIs2[2][2] = 1;
 		expect(findWinner(noWinner)).toBeFalsy();
 	});
 
 	test('findWinner - all directions', () => {
-		const row = [[0, 0, 0], [0, 0, 0], [1, 1, 1]];
+		let board = Array(5).fill(0);
+		for(let i = 0; i < 5; i++) {
+			board[i] = Array(5).fill(0);
+		}
+		
+		let row = board;
+
+		row[0][0] = 1;
+		row[0][1] = 1;
+		row[0][2] = 1;
 		expect(findWinner(row)).toEqual(1);
 
-		const column = [[0, 0, 1], [0, 0, 1], [0, 0, 1]];
+		let column = board;
+		
+		column[0][0] = 1;
+		column[1][0] = 1;
+		column[2][0] = 1;
 		expect(findWinner(column)).toEqual(1);
 
-		const diagonal = [[0, 0, 1], [0, 1, 0], [1, 0, 0]];
+		let diagonal = board;
+		
+		diagonal[0][4] = 2;
+		diagonal[1][3] = 2;
+		diagonal[2][2] = 2;
 		expect(findWinner(diagonal)).toEqual(1);
 	});
 
@@ -53,6 +94,6 @@ describe('Util Functions', () => {
 			board[i] = Array(5).fill(0);
 		}
 
-		expect(getEmptyBoard()).toEqual(board);
+		expect(getEmptyBoard(5)).toEqual(board);
 	});
 });
